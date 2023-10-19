ab3b51f24d800f599a73d171b574cf14d5340af9
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-10-19 17:05:54 +0530

    find single product by id for update product complete

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
 
 require("dotenv").config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@brandshopcluster.dpvzthu.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,51 +19,60 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
+    const brandCollection = client.db("brandDB").collection("brands");
+    const productCollection = client.db("productDB").collection("products");
 
-    const brandCollection = client.db("brandDB").collection("brands")
-    const productCollection = client.db("productDB").collection("products")
-
-    //find multiple brands
+    //find all multiple brands
     app.get("/brands", async (req, res) => {
       const cursor = brandCollection.find();
       const result = await cursor.toArray();
-      res.send(result);
-    })
-
-    // post/insert new single products
-
-    app.post('/products', async (req, res) => {
-      const newProduct = req.body;
-      console.log(newProduct);
-      const result = await productCollection.insertOne(newProduct)
       console.log(result);
       res.send(result);
-      })
+    });
+    // post/insert new single products
 
-// find multiple product
-app.get("/:brandName", async (req, res) => {
-const brand_name = req.params.brandName;
-console.log(brand_name);
-const query = {brand : brand_name}
-const cursor = productCollection.find(query);
-const result = await cursor.toArray();
-  console.log(result);
-  res.send(result);
-}
-)
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      // console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
+      // console.log(result);
+      res.send(result);
+    });
 
+    // find multiple product by brand name
+    app.get("/product/:brandName", async (req, res) => {
+      const brand_name = req.params.brandName;
+      console.log(brand_name);
+      const query = { brand: brand_name };
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    // find single product by id for update product
+    app.get("/product/updateProduct/:brandName/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -73,12 +80,10 @@ const result = await cursor.toArray();
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Brand shop server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Brand shop server is running");
+});
 
 app.listen(port, () => {
-  console.log(`Brand shop server is listening on port ${port}`)
-})
+  console.log(`Brand shop server is listening on port ${port}`);
+});
